c9f1da740812da96df5d60670c016f349add2213
moiv3 <159528029+moiv3@users.noreply.github.com>
2024-11-16 10:18:36 +0800

    update cabinet page

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 app = FastAPI()
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/test")
 def test_page():
     return {"ok": True}
 
-@app.get("/machines")
-def get_machines():
-    return {"ok": True}
+@app.get("/cabinets/list")
+def get_cabinet_list():
+    return {"cabinet_list": ["TDJ0001","TDJ0002","TDJ0003",]}
+
+@app.get("/cabinets/{cabinet_id}")
+def get_cabinet_detail(cabinet_id: str):
+    return {"cabinet_id": cabinet_id, "result": "Nothing here!"}
+
+@app.get("/cabinets")
+def get_cabinet_page():
+    return FileResponse("static/cabinet.html")
